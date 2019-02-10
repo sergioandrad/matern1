76811afd498705d1596252f2da4e815facdb7422
sergioandrad <sergioandrad@users.noreply.github.com>
2019-02-09 23:55:23 -0200

    html
    hmtl gerado no R

---
+++ js/matern1.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#matern1').scianimator({
+	    'images': ['images/matern11.png', 'images/matern12.png', 'images/matern13.png', 'images/matern14.png', 'images/matern15.png', 'images/matern16.png', 'images/matern17.png', 'images/matern18.png', 'images/matern19.png', 'images/matern110.png', 'images/matern111.png', 'images/matern112.png', 'images/matern113.png', 'images/matern114.png', 'images/matern115.png', 'images/matern116.png', 'images/matern117.png', 'images/matern118.png', 'images/matern119.png', 'images/matern120.png', 'images/matern121.png', 'images/matern122.png', 'images/matern123.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#matern1').scianimator('play');
+    });
+})(jQuery);
